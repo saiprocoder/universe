30c8d8bbbbb72263d18a786853d89cd60629d3c4
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2022-02-28 08:15:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var a=prompt("Welcome to Team Gryffindor's project on Black Holes! Type S, to see the Slides. Type W, to see the HTML project.");
 
     if(a=="S") {
-        window.location.href="https://docs.google.com/presentation/d/1x1OxpC_9hln7-HSABcv-poLkoWG0622_co3vJvcNfrQ/edit#slide=id.g35f391192_00"
+        window.location.href="https://docs.google.com/presentation/d/1x1OxpC_9hln7-HSABcv-poLkoWG0622_co3vJvcNfrQ/present?slide=id.g35f391192_00"
     }
 
     if(a=="W") {
