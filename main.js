f9561da4f824b68883f906b9e2d5f47b32a39d1a
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2022-03-07 08:14:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-var a=prompt("Welcome to Team Gryffindor's project on Black Holes! Type S, to see the Slides. Type W, to see the HTML project.");
+console.log("Universe!");
 
-    if(a=="S") {
-        window.location.href="https://docs.google.com/presentation/d/1x1OxpC_9hln7-HSABcv-poLkoWG0622_co3vJvcNfrQ/present?slide=id.g35f391192_00"
-    }
+function Welcome() {
+    var a=prompt("Welcome to Team Gryffindor's project on Universe! Type S, to see the slides. Type W, to see the HTML project.")
+    
+        if(a=="S") {
+            window.location.href="https://docs.google.com/presentation/d/1x1OxpC_9hln7-HSABcv-poLkoWG0622_co3vJvcNfrQ/present?slide=id.g35f391192_00"
+        }
+    
+        if(a=="W") {
+            window.location.href="content.html"
+        }
+    
+        else {
+            alert("Type the correct letter. If you have typed the correct letter, type the correct letter at upper case!");
+            window.location.href="content.html"
+        }
+}
 
-    if(a=="W") {
-        window.location.href="content.html"
-    }
+Welcome();
